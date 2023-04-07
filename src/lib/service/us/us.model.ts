134285a82231b55084173566ff27c2f896c18528
Michael Desigaud <michael.desiagud@redfroggy.fr>
2023-04-07 14:30:59 +0200

    fix(chatgpt): update after comment review

--- src/lib/service/us/us.model.ts
+++ src/lib/service/us/us.model.ts
@@ -1,14 +1,14 @@
-import { CompanyIdType } from "../../../types/company-id-type";
-import { CompanyIdInfoModel } from "../../../types/company-id-info.model";
+import { CompanyIdType } from '../../../types/company-id-type';
+import { CompanyIdInfoModel } from '../../../types/company-id-info.model';
 
 export const COMPANY_US_DATA: CompanyIdInfoModel[] = [
   {
-    name: "EIN",
-    fullName: "Employer Identification Number",
+    name: 'EIN',
+    fullName: 'Employer Identification Number',
     type: CompanyIdType.LOCAL_COMPANY_ID,
-    description: "The EIN is a unique nine-digit number assigned by the IRS to identify businesses for tax purposes.",
-    trustedSourceUrl: "https://www.irs.gov/businesses/small-businesses-self-employed/employer-id-numbers",
-    patterns: ["^[0-9]{2}-[0-9]{7}$"],
+    description: 'The EIN is a unique nine-digit number assigned by the IRS to identify businesses for tax purposes.',
+    trustedSourceUrl: 'https://www.irs.gov/businesses/small-businesses-self-employed/employer-id-numbers',
+    patterns: ['^[0-9]{2}-[0-9]{7}$'],
     parentLevel: true
   }
 ];
